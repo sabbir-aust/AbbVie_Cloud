72754ec201f7843b6d6aa042fea873636378c013
sabbir-aust <sabbir07.ahmed@gmail.com>
2024-11-28 18:15:31 +0600

    Initial Push to Sabbir

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,9 +1,2 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
-
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
